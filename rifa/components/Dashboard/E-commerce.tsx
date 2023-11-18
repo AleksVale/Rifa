2c171a7a7bb45f2949bb2b2b181e46713f6b97bb
Aleksander Ribeiro Vale <aleksander.ribeiro@sga.pucminas.br>
2023-11-18 17:47:55 -0300

    tailadmin

---
+++ rifa/components/Dashboard/E-commerce.tsx
@@ -0,0 +1,25 @@
+'use client'
+import React from 'react'
+import ChartOne from '../Charts/ChartOne'
+import ChartThree from '../Charts/ChartThree'
+import ChartTwo from '../Charts/ChartTwo'
+import ChatCard from '../Chat/ChatCard'
+import TableOne from '../Tables/TableOne'
+import CardDataStats from '../CardDataStats'
+// import Map from "../Maps/TestMap";
+
+// without this the component renders on server and throws an error
+import dynamic from 'next/dynamic'
+const MapOne = dynamic(() => import('../Maps/MapOne'), {
+  ssr: false,
+})
+
+const ECommerce: React.FC = () => {
+  return (
+    <>
+      <div>ola</div>
+    </>
+  )
+}
+
+export default ECommerce
