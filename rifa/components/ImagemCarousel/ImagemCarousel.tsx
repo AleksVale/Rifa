6c263244469d31780b777bb47d3fd48d05a65a77
kauealmeida <kalmeida75@hotmail.com>
2023-11-28 22:16:01 -0300

    geral

--- rifa/components/ImagemCarousel/ImagemCarousel.tsx
+++ rifa/components/ImagemCarousel/ImagemCarousel.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export function ImagemCarousel({ src }: Readonly<{ src: string }>) {
   return (
-    <div className="p-8 w-72 mx-auto">
+    <div className="p-8 w-80 mx-auto justify-center flex px-2">
       <Image alt="" src={src} />
     </div>
   )
