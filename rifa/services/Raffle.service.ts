68c964c1f43f46bb8cf1383b6c310f4eb1792ce7
Aleksander Ribeiro Vale <aleksander.ribeiro@sga.pucminas.br>
2023-12-11 00:54:36 -0300

    Inicio do edit

--- rifa/services/Raffle.service.ts
+++ rifa/services/Raffle.service.ts
@@ -4,6 +4,8 @@ import http from '../hooks/http.common'
 export interface Raffle {
   id: number
   name: string
+  ticketLimit: number
+  price: number
   description: string
   tickets: string
   createdAt: string
@@ -22,5 +24,9 @@ export class RaffleService {
 
   static async create(data: CreateRaffleDTO) {
     return await http.post<Raffle>('raffles', data)
+  }
+
+  static async get(id: string) {
+    return await http.get<Raffle>(`raffles/${id}`)
   }
 }
