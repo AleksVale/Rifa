660377a5223347a6db9b4c1a3640400c54c1e7a6
Aleksander Ribeiro Vale <aleksander.vale@dysrup.com.br>
2023-12-27 02:41:12 -0300

    Refactor TailwindBadge component and add dropdown menu to TransactionInfo

--- rifa/components/TailwindBadge/index.tsx
+++ rifa/components/TailwindBadge/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 interface TailwindBadgeProps {
-  color: string
+  color: 'yellow' | 'green'
   text: string
 }
 
@@ -9,9 +9,14 @@ export default function TailwindBadge({
   color,
   text,
 }: Readonly<TailwindBadgeProps>) {
+  const colorVariants = {
+    green: 'bg-green-50 text-green-600 ring-green-500/10',
+    yellow: 'bg-yellow-50 text-yellow-600 ring-yellow-500/10',
+    red: 'bg-red-50 text-red-600 ring-red-500/10',
+  }
   return (
     <span
-      className={`inline-flex items-center rounded-md bg-${color}-50 px-2 py-1 text-xs font-medium text-${color}-600 ring-1 ring-inset ring-${color}-500/10`}
+      className={`${colorVariants[color]} inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset`}
     >
       {text}
     </span>
--- rifa/components/TransactionInfo/index.tsx
+++ rifa/components/TransactionInfo/index.tsx
@@ -1,8 +1,10 @@
 import { Ticket } from '@/services/Raffle.service'
 import { Buyer } from '@/services/ticket.service'
 import { Transaction } from '@/services/transaction.service'
-import React from 'react'
+import { Fragment, useEffect, useRef, useState } from 'react'
 import TailwindBadge from '../TailwindBadge'
+import { FaEllipsisVertical } from 'react-icons/fa6'
+import { Menu, Transition } from '@headlessui/react'
 
 interface TransactionInfoProps {
   transaction: Transaction
@@ -24,17 +26,38 @@ export default function TransactionInfo({ transaction }: TransactionInfoProps) {
             text={isPending ? 'Pendente' : 'Pago'}
           />
         </div>
-        <div data-headlessui-state="" className="relative">
-          <button
-            className="button px-2"
-            aria-haspopup="true"
-            aria-expanded="false"
-            type="button"
+        <Menu as="div" className="relative inline-block text-left">
+          <div>
+            <Menu.Button className="inline-flex w-full justify-center rounded-md border border-slate-100 bg-white/20 px-2 py-1 text-sm font-medium hover:bg-white/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
+              <FaEllipsisVertical />
+            </Menu.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
           >
-            {/* Add your button content or icon here */}
-          </button>
-          {/* Add your dropdown content here */}
-        </div>
+            <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+              <div className="px-1 py-1 ">
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      className={`${
+                        active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      Edit
+                    </button>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
       </div>
       <div className="relative">
         <hr className="my-3.5 block sm:hidden" />
